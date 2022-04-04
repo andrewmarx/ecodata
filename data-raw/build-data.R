2294b9fc4547677b421d45b12c11f4906590ff3c
Andrew Marx <andrewjmarx@gmail.com>
2022-04-04 12:50:19 -0400

    Revert "Split data related code to separate repository"
    This reverts commit 14630f2b2ececd184621bfdc0357069f9a88c548.

---
+++ data-raw/build-data.R
@@ -0,0 +1,24 @@
+library(usethis)
+
+# Internal data
+._subdisciplines <- read.csv("data-raw/_subdisciplines.csv", stringsAsFactors = FALSE)
+._topics <- read.csv("data-raw/_topics.csv", stringsAsFactors = FALSE)
+._st_pivot <- read.csv("data-raw/_st_pivot.csv", stringsAsFactors = FALSE)
+._data_sets <- read.csv("data-raw/_data_sets.csv", stringsAsFactors = FALSE)
+._td_pivot <- read.csv("data-raw/_td_pivot.csv", stringsAsFactors = FALSE)
+
+use_data(._subdisciplines,
+         ._topics,
+         ._st_pivot,
+         ._data_sets,
+         ._td_pivot,
+         internal = TRUE, overwrite = TRUE)
+
+
+# Data sets
+for (f in list.files("data-raw/data", "^[a-z][a-z0-9_]*[a-z0-9]\\.csv$")) {
+  name = sub(pattern = "\\.csv$", "", f)
+  data = read.csv(paste0("data-raw/data/", f), stringsAsFactors = FALSE)
+  assign(name, data)
+  do.call("use_data", list(as.name(name), overwrite = TRUE))
+}
