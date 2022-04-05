3642be84b8708f383a0e1ced3ff8ba0a94499110
Andrew Marx <andrewjmarx@gmail.com>
2022-04-05 10:52:23 -0400

    Add example script

---
+++ inst/examples/example.R
@@ -0,0 +1,14 @@
+# Try running the ecodata() without any arguments. You'll get some information
+# about valid options for the first parameter.
+data <- ecodata::ecodata()
+
+# Note that the function returns NULL at this point
+print(data)
+
+
+# Repeat this process until we get data
+data <- ecodata::ecodata("Quantitative Ecology")
+data <- ecodata::ecodata("Quantitative Ecology", "Normal Distribution")
+data <- ecodata::ecodata("Quantitative Ecology", "Normal Distribution", "Normal Random Sets")
+
+head(data)
