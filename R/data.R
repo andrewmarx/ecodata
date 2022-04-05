e855862c1ac1e81dc93325f25eb488d9de9b7030
Andrew Marx <andrewjmarx@gmail.com>
2022-04-05 18:20:11 -0400

    Add data file

--- R/data.R
+++ R/data.R
@@ -1,2 +1,18 @@
-# Copyright (c) 2019 Andrew Marx. All rights reserved.
+# Copyright (c) 2022 Andrew Marx. All rights reserved.
 # Licensed under GPLv3.0. See LICENSE file in the project root for details.
+
+#' Lepanthes rupestris
+#'
+#' Description goes here
+#'
+#' @format A data frame with 975 rows and 23 variables:
+#' \describe{
+#'   \item{UID}{}
+#'   \item{x.coord}{}
+#' }
+#'
+#' @source
+#' Acevedo, Miguel; Beaudrot, Lydia; Melendez-Ackerman, Elvia; Tremblay, Raymond (2020), Local extinction risk under climate change in a neotropical asymmetrically dispersed epiphyte, Dryad, Dataset, https://doi.org/10.5061/dryad.9p8cz8wc6
+#'
+#' \url{https://doi.org/10.5061/dryad.9p8cz8wc6}
+"lepanthes_rupestris"
